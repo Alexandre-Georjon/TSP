2ca31c11db2530dc306aad41722448031281c94d
Alexandre Georjon <alexandre.georjon@imt-atlantique.net>
2018-09-28 16:52:15 +0200

    modif test

--- src/tsp/TSPSolver.java
+++ src/tsp/TSPSolver.java
@@ -78,7 +78,7 @@ public class TSPSolver {
 		{
 			// TODO
 			// Code a loop base on time here
-			// Test
+			// Test test
 			spentTime = System.currentTimeMillis() - startTime;
 		}while(spentTime < (m_timeLimit * 1000 - 100) );
 		
