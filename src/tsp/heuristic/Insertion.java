240fd402726e7250f65e7fc769df9eb3cf5fe596
borisdaniel10 <bd.lancheros10@uniandes.edu.co>
2018-10-24 03:03:45 -0500

    deuxième partie de la méthode d'insertion.

--- src/tsp/heuristic/Insertion.java
+++ src/tsp/heuristic/Insertion.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 
 import tsp.Instance;
 
-	// Nous déclarons la méthode d'insertion
+	/** Nous déclarons la méthode d'insertion
+	 * @author Lancheros-
+	 */
+	 
 public class Insertion extends AHeuristic {
 
 	public Insertion(Instance instance, String name) throws Exception {
@@ -12,7 +15,10 @@ public class Insertion extends AHeuristic {
 		// TODO Auto-generated constructor stub
 	}
 
-	// À partir du dépôt 0, nous déterminons le nœud le plus proche "C" 
+	/** À partir du dépôt 0, nous déterminons le nœud le plus proche "C"
+	 * @param i, nonvisite 
+	 * @author Lancheros-
+	 * */  
 	
 	public int getplusproche(int i,ArrayList<Integer> nonvisite) {
 		long[] distancei=this.m_instance.getDistances()[i];
@@ -29,7 +35,9 @@ public class Insertion extends AHeuristic {
 		return min;
 	}
 	
-	// Nous avons construit le subtour 0-c-0
+	/** Nous avons construit le subtour 0-c-0
+	 * @author Lancheros
+	 *  */ 
 	
 	public void solve() throws Exception {
 		ArrayList<Integer> nonvisite=new ArrayList<Integer>(this.m_instance.getNbCities());
@@ -38,37 +46,55 @@ public class Insertion extends AHeuristic {
 		}
 		int villeprecedente=0;
 		int villesuivante=0;
-		for(int k=0;k<this.m_instance.getNbCities()-1;k++) {
+		int villefinal = 0;
+					
+			// se construye el arreglo 0-c-0
 			System.err.println(villeprecedente);
 			villesuivante=this.getplusproche(villeprecedente, nonvisite);
 			System.err.println(villesuivante);
+			System.err.println(villefinal);
 			System.err.println(nonvisite);
 			villeprecedente=villesuivante;
 
-			this.m_solution.setCityPosition(villesuivante, k+1);
+			this.m_solution.setCityPosition(villesuivante,1);
+			 
+			//  Nous trouvons le nœud "K" le plus proche (qui n'est pas dans le subtour) 
 			
-			/*Nous trouvons le nœud "K" le plus proche (qui n'est pas dans le subtour)
-		 	à n'importe quel nœud du subtour.*/
+			for(int k=1;k<this.m_instance.getNbCities()-1;k++) {
+				
+				System.err.println(villeprecedente);
+				villesuivante=this.getplusproche(villeprecedente, nonvisite);
+				System.err.println(villesuivante);
+				System.err.println(nonvisite);
+				villeprecedente=villesuivante;
+
+				this.m_solution.setCityPosition(villesuivante,1);
+				
+			}
 			
 			
 			
 			
+		 	
 			
 			
-			
-			
-			
-		}
+		
 	
 	
 	
 	
 	
 	
-	/*On trouve l'arc (i, j) du subtour qui permet l'insertion la plus 
-	 économique dans le nœud K
+	/** 
+	 * On trouve l'arc (i, j) du subtour qui permet l'insertion la plus 
+	 * économique dans le nœud K 
+	 * coût d'insertion = d(i,k) + d(k,j)
+	 *  @author Lancheros - 
+	 *  */
 	 
-	 coût d'insertion = d(i,k) + d(k,j) */
+	 
+	 
+	 
 	
 	
 	
