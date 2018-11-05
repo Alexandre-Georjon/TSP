127cb7dda2e9ae9223aaa5ff94b2def1803c6faa
Alexandre Georjon <alexandre.georjon@imt-atlantique.net>
2018-11-05 22:13:54 +0100

    change

--- src/tsp/metaheuristic/Genetique.java
+++ src/tsp/metaheuristic/Genetique.java
@@ -10,18 +10,40 @@ import java.util.Random;
 import tsp.Instance;
 import tsp.Solution;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Genetique.
+ */
 public class Genetique extends AMetaheuristic{
 
+	/**
+	 * Instantiates a new genetique.
+	 *
+	 * @param instance the instance
+	 * @param name the name
+	 * @throws Exception the exception
+	 */
 	public Genetique(Instance instance, String name) throws Exception {
 		super(instance, name);
 		// TODO Auto-generated constructor stub
 	}
 
+	/* (non-Javadoc)
+	 * @see tsp.metaheuristic.AMetaheuristic#solve(tsp.Solution)
+	 */
 	@Override
 	public Solution solve(Solution sol) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	/**
+	 * Generate a new population of solutions
+	 *
+	 * @param n the number of solutions wanted
+	 * @return the solution[]
+	 * @throws Exception the exception
+	 */
 	public Solution[] genererpopulation(int n) throws Exception {
 		Solution[] population=new Solution[n];
 		Solution soluce=new Solution(m_instance);
@@ -38,16 +60,19 @@ public class Genetique extends AMetaheuristic{
 				int temp=population[k].getCity(j);
 				population[k].setCityPosition(population[k].getCity(i), j);
 				population[k].setCityPosition(temp, i);
-				//System.err.println(i);
 			}
-			//for(int l=0;l<=this.m_instance.getNbCities();l++) {
-			//	System.err.print(population[k].getCity(l)+" ");
-			//}
-			//System.err.println("");
+			
 			
 		}
 		return population;
 	}
+	
+	/**
+	 * Selection : evaluate and sort population based on their length.
+	 *
+	 * @param population the population
+	 * @throws Exception the exception
+	 */
 	public void selection(Solution[] population) throws Exception {
 		double eval=0;
 		for(Solution s:population) {
@@ -55,53 +80,16 @@ public class Genetique extends AMetaheuristic{
 		}
 		 Arrays.sort(population);
 	}
-	public Solution[] hybridation1(Solution parent1,Solution parent2) throws Exception {
-		Solution fils1= new Solution(m_instance);
-		Solution fils2= new Solution(m_instance);
-		ArrayList<Integer> nonvisite1=new ArrayList<Integer>();
-		ArrayList<Integer> nonvisite2=new ArrayList<Integer>();
-		int tailleSolution=parent1.getInstance().getNbCities();
-		int index1=0;
-		int index2=0;
-		boolean trouver1=false;
-		boolean trouver2=false;
-		for(int i=0;i<tailleSolution/2;i++) {
-			fils1.setCityPosition(parent1.getCity(i), i);
-			fils2.setCityPosition(parent2.getCity(i), i);			
-		}
-		for(int i=tailleSolution/2;i<tailleSolution;i++) {
-			nonvisite1.add(parent1.getCity(i));
-			nonvisite2.add(parent2.getCity(i));
-		}
-		for(int i=tailleSolution/2;i<tailleSolution;i++) {
-			while(!trouver1) {
-				if(nonvisite1.contains(parent2.getCity(index2))){
-					fils1.setCityPosition(parent2.getCity(index2), i);
-					nonvisite1.remove(nonvisite1.indexOf(parent2.getCity(index2)));
-					trouver1=true;
-				}else {
-					index2++;
-				}
-			}
-			while(!trouver2) {
-				if(nonvisite2.contains(parent1.getCity(index1))){
-					fils2.setCityPosition(parent1.getCity(index1), i);
-					nonvisite2.remove(nonvisite2.indexOf(parent1.getCity(index1)));
-					trouver2=true;
-				}else {
-					index1++;
-				}
-			}
-			trouver1=false;
-			trouver2=false;
-			
-		}
-		double eval1=fils1.evaluate();
-		double eval2=fils2.evaluate();
-		Solution[] res={fils1,fils2};
-		return res;
-	}
-	public Solution[] hybridation2(Solution parent1,Solution parent2) throws Exception {
+	
+	/**
+	 * Hybridation : create a news solution mixing two solutions
+	 *
+	 * @param parent1 the parent 1
+	 * @param parent2 the parent 2
+	 * @return the solution[]
+	 * @throws Exception the exception
+	 */
+	public Solution[] hybridation(Solution parent1,Solution parent2) throws Exception {
 		Solution fils1= new Solution(m_instance);
 		Solution fils2= new Solution(m_instance);
 		Solution fils3= new Solution(m_instance);
@@ -184,6 +172,14 @@ public class Genetique extends AMetaheuristic{
 		return res;
 	}
 	
+	/**
+	 * Hybridation same as hybridation but using the partially mapped hybridation algorithm
+	 *
+	 * @param parent1 the parent 1
+	 * @param parent2 the parent 2
+	 * @return the solution
+	 * @throws Exception the exception
+	 */
 	public Solution hybridationPMX(Solution parent1,Solution parent2) throws Exception {
 		Solution fils = new Solution(m_instance);
 		Random rnd=new Random();
@@ -224,6 +220,12 @@ public class Genetique extends AMetaheuristic{
 	
 	
 	
+	/**
+	 * Mutation, mutate a solution swaping two cities
+	 *
+	 * @param sol the solution to be mutated
+	 * @throws Exception the exception
+	 */
 	public void mutation(Solution sol) throws Exception {
 		Random rnd = new Random();
 		int j=rnd.nextInt(sol.getInstance().getNbCities()-2)+1;
@@ -232,6 +234,14 @@ public class Genetique extends AMetaheuristic{
 		sol.setCityPosition(sol.getCity(j),i);
 		sol.setCityPosition(ville, j);
 	}
+	
+	/**
+	 * Mutation select a pourcentage % of the population to be mutated
+	 *
+	 * @param population the population
+	 * @param pourcentage the pourcentage
+	 * @throws Exception the exception
+	 */
 	public void mutation(Solution[] population,double pourcentage) throws Exception {
 		for(int k=0;k<population[0].getInstance().getNbCities()*pourcentage;k++) {
 			Random rnd = new Random();
@@ -240,6 +250,13 @@ public class Genetique extends AMetaheuristic{
 		}
 	}
 	
+	/**
+	 * Create a new generation of the population
+	 *
+	 * @param population the population
+	 * @return the population
+	 * @throws Exception the exception
+	 */
 	public Solution[] evolution(Solution[] population) throws Exception {
 		selection(population);
 		Solution[] nouvellepopulation=new Solution[population.length];
@@ -248,7 +265,7 @@ public class Genetique extends AMetaheuristic{
 			nouvellepopulation[k]=population[k].copy();
 		}
 		for(int k=0;k+1<milieu;k++) {
-			Solution temp=hybridation1(population[k], population[k+1])[0];
+			Solution temp=hybridation(population[k], population[k+1])[0];
 			if(temp.compareTo(population[k])<0) {
 				nouvellepopulation[milieu+k]=temp;
 			}else {
@@ -260,6 +277,13 @@ public class Genetique extends AMetaheuristic{
 	}
 	
 	
+	/**
+	 * Generated a new population out of the ancient one using probabilities to select solution used to hybridate
+	 *
+	 * @param population the population
+	 * @return the new generation
+	 * @throws Exception the exception
+	 */
 	public Solution[] evolutionalternative(Solution[] population) throws Exception {
 		selection(population);
 		Solution[] nouvellepopulation=new Solution[population.length];
