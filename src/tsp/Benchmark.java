66cdf9ef5dae5fc52eb460f3810cff9c88660846
Serge <Serge@172.17.2.30>
2018-11-05 10:28:14 +0100

    benchmark

---
+++ src/tsp/Benchmark.java
@@ -0,0 +1,36 @@
+package tsp;
+import java.io.IOException;
+
+import tsp.heuristic.Plus_proche_voisin;
+public class Benchmark {
+
+	public static Instance[] buildInstances() throws IOException {
+		Instance[] inst = new Instance[12];
+		inst[0] = new Instance("instances/brazil58.tsp",1);
+		inst[1] = new Instance("instances/d198.tsp",0);
+		inst[2] = new Instance("instances/d657.tsp",0);
+		inst[3] = new Instance("instances/eil10.tsp",0);
+		inst[4] = new Instance("instances/eil101.tsp",0);
+		inst[5] = new Instance("instances/eil51.tsp",0);
+		inst[6] = new Instance("instances/kroA100.tsp",0);
+		inst[7] = new Instance("instances/kroA150.tsp",0);
+		inst[8] = new Instance("instances/kroA200.tsp",0);
+		inst[9] = new Instance("instances/lin318.tsp",0);
+		inst[10] = new Instance("instances/pcb442.tsp",0);
+		inst[11] = new Instance("instances/rat575.tsp",0);
+		return inst;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Instance[] instances = buildInstances();
+		for(int i = 0; i<instances.length; i++) {
+			Plus_proche_voisin prochevoisin=new Plus_proche_voisin(instances[i], "plusprochevoisin");
+			prochevoisin.solve();
+			System.err.println(prochevoisin.getSolution().evaluate());
+			
+			
+			
+		}
+	}
+	
+}
